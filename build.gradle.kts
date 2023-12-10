33536cabc4a5cc15ab4b0ee4c6344513655e5945
Antoine <antoine@enimaloc.fr>
2023-12-10 13:27:25 +0100

    deps: add Minestom-ce dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
+    // Minestom - base
+    implementation("dev.hollowcube:minestom-ce:8715f4305d") // using forked version of Minestom for 1.20.1
+
+    // Testing
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
